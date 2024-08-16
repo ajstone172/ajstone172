85971df19e9d6b2583fec612573fcc9371d6146b
ajstone172 <ajstone172@gmail.com>
2024-08-16 17:43:38 -0400

    Add files via upload

---
+++ hbd_madlib.py
@@ -0,0 +1,15 @@
+age = input("How old are you? ")
+age = int(age)
+if age < 35:
+    print("You're a young whipper-snapper!")
+elif age < 62:
+    print("You're middle aged")
+else:
+    print("You're really old!!!")
+
+\
+\
+\
+verb1 = input("Type a verb, 1st person singular present tense: ")
+
+print(f"It's my birthday, and I'll {verb1} if I want to \r\n {verb1} if I want to \r\n {verb1} if I want to \r\n You would {verb1} too if it happened to you!")
